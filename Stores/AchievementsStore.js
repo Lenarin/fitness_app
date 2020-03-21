af2850c22ce8931b46d747a61d0e453f3e06c423
Soarex16 <soarex16@gmail.com>
2020-03-21 18:03:24 +0300

    remove mock data

--- Stores/AchievementsStore.js
+++ Stores/AchievementsStore.js
@@ -1,27 +1,11 @@
 import { observable, action } from 'mobx';
 import { persist, create } from 'mobx-persist';
 import { AsyncStorage } from 'react-native';
-import Achievement from './Models/Achievements';
 
 class AchievementsStore {
     @persist('list')
     @observable
     Achievements = [];
-
-    // Mock achieves
-    constructor() {
-        this.Achievements = [
-            new Achievement(new Date('2020-03-01'), 1, 'file', '#151697', 'You achived eternity!', 'WAAAAGH'),
-            new Achievement(new Date('2020-03-03'), 1, 'file', '#151697', 'You achived eternity!', 'WAAAAGH'),
-            new Achievement(new Date('2020-03-04'), 1, 'file', '#151697', 'You achived eternity!', 'WAAAAGH'),
-            new Achievement(new Date('2020-03-01'), 1, 'food', '#151697', 'All dinosawrs died!', 'No lizards for you'),
-            new Achievement(new Date('2020-03-01'), 1, 'food', '#151697', 'All dinosawrs died!', 'No lizards for you'),
-            new Achievement(new Date('2020-03-01'), 1, 'food', '#151697', 'All dinosawrs died!', 'No lizards for you'),
-            new Achievement(new Date('2020-03-01'), 1, 'food', '#151697', 'All dinosawrs died!', 'No lizards for you'),
-            new Achievement(new Date('2020-03-01'), 1, 'food', '#151697', 'All dinosawrs died!', 'No lizards for you'),
-            new Achievement(new Date('2020-03-01'), 1, 'food', '#151697', 'All dinosawrs died!', 'No lizards for you'),
-        ];
-    }
 
     @action
     addAchievement(achievement) {
